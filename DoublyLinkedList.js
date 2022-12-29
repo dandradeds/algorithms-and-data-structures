7cfb54012e656b62d739b480d7005b1982ff474b
dandradeds <daniel.andrade5554@gmail.com>
2022-12-29 15:48:16 -0300

    Adiction DoublyLinkedList

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -1,11 +1,13 @@
-function LinkedList(){
+function DoublyLinkedList(){
     var Node = function(element) {
         this.element = element
         this.next = null
+        this.prev = null
     }
 
     var length = 0
     var head = null
+    var tail = null
 
     this.append = function(element){
         var node = new Node(element), 
@@ -13,28 +15,40 @@ function LinkedList(){
 
         if(head === null){ 
             head = node 
+            tail = node
         } else {
             current = head
 
             while(current.next) {
                 current = current.next
             }
-
             current.next = node
+            tail = node
         }
         length++
     }
 
     this.insert = function(position, element){
         if(position >= 0 && position <= length){
-            var node = new Node(element)
-            current = head
+            var node = new Node(element),
+            current = head,
             previous,
             index = 0
 
             if(position === 0){
-                node.next = current
-                head = node
+                if(!head){
+                    head = node
+                    tail = node
+                } else {
+                    node.next = current
+                    current.prev = node
+                    head = node
+                }
+            } else if(position === length){
+                current = tail
+                current.next = node
+                node.prev = current
+                tail = node
             } else {
                 while(index++ < position){
                     previous = current
@@ -42,28 +56,41 @@ function LinkedList(){
                 }
                 node.next = current
                 previous.next = node
+                current.prev = node
+                node.prev = previous
             }
-            length++ 
-            return true 
+            length++
+            return true
         } else {
             return false
         }
     }
 
-    this.removeAt = function(position){
-        if(position > - 1 && position < length){
-            var current = head,
+    this.removeAt = function(position){//2
+        if(position > - 1 && position < length){//true
+            var current = head, //joao
             previous,
             index = 0
 
-            if(position === 0){
-                head = current.next
-            } else {
-                while(index++ < position){ //  [j, jose, m] 2
-                    previous = current //jose
-                    current = current.next //maria
+            if(position === 0){ 
+                head = current.next 
+
+                if(length === 1){
+                    tail = null
+                } else {
+                    head.prev = null
                 }
-                previous.next = current.next //jose
+            } else if(position === length - 1){ 
+                current = tail 
+                tail = current.prev 
+                tail.next = null 
+            } else {
+                while(index++ < position){ //2 < 2
+                    previous = current //Ana
+                    current = current.next //Jose
+                }
+                previous.next = current.next //Maria
+                current.next.prev = previous 
             }
             length--
             return current.element
@@ -114,7 +141,6 @@ function LinkedList(){
         }
 
         return string
-
     }
 
     this.print = function(){
@@ -122,3 +148,18 @@ function LinkedList(){
 
     }
 }
+
+var dll = new DoublyLinkedList()
+dll.append('João')
+dll.append('José')
+dll.append('Maria')
+dll.insert(0, 'Carlos')
+dll.insert(4, 'Lucas')
+dll.insert(2, 'Ana')
+dll.print()
+dll.removeAt(0)
+dll.print()
+dll.removeAt(4)
+dll.print()
+dll.removeAt(2)
+dll.print()
