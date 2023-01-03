86c1e530152e4aa82ebcf785cd7d8b3d60943845
dandradeds <daniel.andrade5554@gmail.com>
2023-01-03 17:06:10 -0300

    implemented bfs method

--- Graph.js
+++ Graph.js
@@ -1,40 +1,40 @@
-function Dictionary(){
+function Dictionary() {
     var items = {}
 
-    this.set = function(key, value){
+    this.set = function(key, value) {
         items[key] = value
     }
 
-    this.delete = function(key){
-        if(this.has(key)){
+    this.delete = function(key) {
+        if(this.has(key)) {
             delete items[key]
             return true
         }
         return false
     }
 
-    this.has = function(key){
+    this.has = function(key) {
         return items.hasOwnProperty(key)
     }
 
-    this.get = function(key){
+    this.get = function(key) {
         return this.has(key) ? items[key] : undefined
     }
 
-    this.clear = function(){
-        items = {} 
+    this.clear = function() {
+        items = {}
     }
 
-    this.size = function(){
+    this.size = function() {
         return Object.keys(items).length
     }
 
-    this.keys = function(){
+    this.keys = function() {
         return Object.keys(items)
     }
 
-    this.value = function(){
-        var values = []
+    this.values = function() {
+        var values = [],
         keys = Object.keys(items)
 
         for(var i = 0; i < keys.length; i++) {
@@ -43,42 +43,102 @@ function Dictionary(){
         return values
     }
 
-    this.getItems = function(){
+    this.getItems = function() {
         return items
     }
 }
 
-function Graph(){
+function Queue() {
+    var items = []
+
+    this.enqueue = function(element) {
+        items.push(element)
+    }
+
+    this.dequeue = function() {
+        return items.shift()
+    }
+
+    this.front = function() {
+        return items[0]
+    }
+
+    this.isEmpty = function() {
+        return items.length === 0
+    }
+
+    this.size = function() {
+        return items.length
+    }
+
+    this.print = function() {
+        console.log(items.toString())
+    }
+}
+
+function Graph() {
     var vertices = []
     var adjList = new Dictionary()
 
-    this.addVertex = function(v){   
-        vertices.push(v)
-        adjList.set(v, [])
-
+    this.addVertex = function(v) {
+        vertices.push(v) //myVertices
+        adjList.set(v, []) //myVertices e array vazio 
     }
 
-    this.addEdge = function(v, w){
+    this.addEdge = function(v, w) {
         adjList.get(v).push(w)
         adjList.get(w).push(v)
-
     }
 
-    this.toString = function(){
+    this.toString = function() {
         var s = ''
-        for(var i = 0; i < vertices.length; i++){
+        for(var i = 0; i < vertices.length; i++) {
             s += vertices[i] + ' -> '
             var neighbors = adjList.get(vertices[i])
-            for(var j = 0; j < neighbors.length; j++){
+            for(var j = 0; j < neighbors.length; j++) {
                 s += neighbors[j] + ' '
             }
             s += '\n'
         }
+        return s
+    }
+
+    var initializeColor = function() {
+        var color = []
+        for(var i = 0; i < vertices.length; i++){
+            color[vertices[i]] = 'white'
+        }
+        return color 
+    }
+
+    this.bfs = function(v, callback){
+        var color = initializeColor(),
+        queue = newQueue();
+        queue.enqueue()
+
+        while(!queue.isEmpty()){
+            var u = queue.dequeue()
+            neighbors = adjList.get(u)
+            color[u] = 'grey'
+            for(var i = 0;i < neighbors.length; i++){
+                var w = neighbors[i]
+                if(color[w] === 'white'){
+                    color[w] = 'grey'
+                    queue.enqueue(w)
+                }
+            }
+            color[u] = 'black'
+            callback[u]
+        }
     }
 }
 
+function printNode(value){
+    console.log('Visited vertex: ' + value);
+}
+
 var graph = new Graph()
-var myVertices = ['A', 'B','C','D','E','F','G','H','I',]
+var myVertices = ['A', 'B','C','D','E','F','G','H','I']
 for(var i = 0; i < myVertices.length; i++){
     graph.addVertex(myVertices[i])
 }
@@ -94,4 +154,4 @@ graph.addEdge('B', 'E')
 graph.addEdge('B', 'F')
 graph.addEdge('E', 'I')
 
-console.log(graph.toString());
+console.log(graph.toString())
