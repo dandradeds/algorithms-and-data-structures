4ae7531e6efdfced46de20416cf21f0cbdc931a2
dandradeds <daniel.andrade5554@gmail.com>
2023-01-03 14:45:05 -0300

    adiction Graph file

---
+++ Graph.js
@@ -0,0 +1,74 @@
+function Dictionary(){
+    var items = {}
+
+    this.set = function(key, value){
+        items[key] = value
+    }
+
+    this.delete = function(key){
+        if(this.has(key)){
+            delete items[key]
+            return true
+        }
+        return false
+    }
+
+    this.has = function(key){
+        return items.hasOwnProperty(key)
+    }
+
+    this.get = function(key){
+        return this.has(key) ? items[key] : undefined
+    }
+
+    this.clear = function(){
+        items = {} 
+    }
+
+    this.size = function(){
+        return Object.keys(items).length
+    }
+
+    this.keys = function(){
+        return Object.keys(items)
+    }
+
+    this.value = function(){
+        var values = []
+        keys = Object.keys(items)
+
+        for(var i = 0; i < keys.length; i++) {
+            values.push(items[keys[i]])
+        }
+        return values
+    }
+
+    this.getItems = function(){
+        return items
+    }
+}
+
+function Graph(){
+    this.addVertex = function(v){   
+        var vertices = []
+        var adjList = new Dictionary()
+    }
+
+    this.addEdge = function(v, w){
+        adjList.get(v).push(w)
+        adjList.get(w).push(v)
+
+    }
+
+    this.toString = function(){
+        var s = ''
+        for(var i = 0; i < vertices.length; i++){
+            s += vertices[i] + ' -> '
+            var neighbors = adjList.get(vertices[i])
+            for(var j = 0; j < neighbors.length; j++){
+                s += neighbors[j] + ' '
+            }
+            s += '\n'
+        }
+    }
+}
