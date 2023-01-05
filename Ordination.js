75cb0c251fb2ff28df83776a578b7470a0f27529
dandradeds <daniel.andrade5554@gmail.com>
2023-01-05 16:39:28 -0300

    implementation of insertionSort

--- Ordination.js
+++ Ordination.js
@@ -37,6 +37,30 @@ function ArrayList() {
         }
     }
 
+
+    //[5,4,3,2,1]
+    //[4,5,3,2,1]
+    //[4,3,5,2,1]
+    //[3,4,5,2,1]
+    //[3,4,2,5,1]
+    //[3,2,4,5,1]
+    //[2,3,4,5,1]
+    //[1,2,3,4,5]
+
+    this.insertionSort = function(){
+        var j, temp 
+        for(var i = 1; i < array.length; i++){ //4
+            j = i //[4]
+            temp = array[i] //1
+//                 [1]            2        1
+            while(j > 0 && array[j - 1] > temp){ 
+                array[j] = array[j - 1] // 2
+                j-- // 0
+            }
+            array[j] = temp //
+        }
+    }
+
     var swap = function(array, index1, index2){ 
         var aux = array[index1] 
         array[index1] = array[index2] 
@@ -52,5 +76,6 @@ al.insert(2)
 al.insert(1)
 
 //al.bubbleSort()
-al.selectionSort()
+//al.selectionSort()
+al.insertionSort()
 console.log(al.toString());
