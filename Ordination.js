3856eda5a247baaa0bb71b5cb3026b21693d2758
dandradeds <daniel.andrade5554@gmail.com>
2023-01-06 16:13:02 -0300

    quickSort add

--- Ordination.js
+++ Ordination.js
@@ -55,40 +55,77 @@ function ArrayList() {
     }
 
 
-    var mergeSortRec = function(array){
+    var mergeSortRec = function(array){ 
         var length = array.length 
-        if(length === 1) {
+        if(length === 1) { 
             return array 
         }
-        var mid = Math.floor(length / 2), 
+        var mid = Math.floor(length / 2),
         left = array.slice(0, mid), 
-        rigth = array.slice(mid, length) 
+        rigth = array.slice(mid, length)
 
         return merge(mergeSortRec(left), mergeSortRec(rigth))
-    }
+    }                          
 
-    var merge = function(left, rigth){
-        var result = [],
+    var merge = function(left, rigth){ 
+        var result = [], 
         il = 0,
-        ir = 0
+        ir = 0 
 
-        while(il < left.length && ir < rigth.length){
-            if(left[il] < rigth[ir]){
+        while(il < left.length && ir < rigth.length){ 
+            if(left[il] < rigth[ir]){ 
                 result.push(left[il++])
-            } else {
-                result.push(rigth[ir++])
+            } else { 
+                result.push(rigth[ir++]) 
             }
         }
-
-        while(il < left.length){
-            result.push(left[il++])
+        while(il < left.length){ 
+            result.push(left[il++]) 
         }
 
-        while(ir < rigth.length){
+        while(ir < rigth.length){ 
             result.push(rigth[ir++])
         }
 
         return result
+    }
+
+    this.quickSort = function() {
+        quick(array, 0, array.length - 1)
+    }
+
+    var quick = function(array, left, right){
+        var index
+        if(array.length > 1){
+            index = partition(array,left,right)
+            if(left < index - 1){
+                quick(array,left, index - 1)
+            }
+            if(index < right){
+                quick(array, index, right)
+            }
+        }
+    }
+
+    var partition = function(array, left, right){
+        var pivot = array[Math.floor((right + left) / 2)],
+        i = left,
+        j = right
+
+        while(i <= j){
+            while(array[i] < pivot){
+                i++
+            }
+            while(array[j] > pivot){
+                j--
+            }
+        }
+        if(i <= j){
+            swap(array, i, j)
+            i++
+            j--
+        }
+        return i 
     }
 
     var swap = function(array, index1, index2){ 
@@ -108,7 +145,5 @@ al.insert(4)
 al.insert(3)
 al.insert(2)
 al.insert(1)
-
-al.mergeSort()
 
 console.log(al.toString());
